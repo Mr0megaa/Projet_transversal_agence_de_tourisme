807e385075940356eb6caafb1114be4e7e294a0f
Mr0megaa <rousselthomas28@gmail.com>
2025-11-17 01:26:13 +0100

    Lien entre login et admin

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
+<?php
+
+$mail = $_POST["email"];
+$pass = $_POST["password"];
+
+if ($mail == "admin@admin" && $pass == "password") {
+  header("Location: admin.html"); //Redirige en PHP
+  exit();
+} else {
+  echo "mdp incorrect"; //fonctionne pas (surement parce que je lui ai pas dit ou l'écrire)
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +23,7 @@
 </head>
 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top ">
   <div class="container-fluid ">
-    <a class="navbar-brand orange gras josefin color-principal" href="#">Voyage-Voyage</a>
+    <a class="navbar-brand orange gras josefin color-principal" href="index.html">Voyage-Voyage</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -19,13 +31,13 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link fw-bold black margin10 josefin color-principal" href="#">DESTINATION</a>
+          <a class="nav-link fw-bold black margin10 josefin color-principal" href="#index.html">DESTINATION</a>
         </li>
         <li class="nav-item">
           <a class="nav-link fw-bold black margin10 josefin color-principal" href="#">CIRCUIT</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link fw-bold black margin10 josefin color-principal" href="login.html">PROFIL</a>
+          <a class="nav-link fw-bold black margin10 josefin color-principal" href="login.php">PROFIL</a>
         </li>
       </ul>
     </div>
@@ -39,14 +51,15 @@
         <div class="card shadow-sm">
           <div class="card-body">
             <h3 class="card-title text-center mb-4">Sign In</h3>
-            <form>
+            <form method="POST" action="">
               <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
+                <!-- name="email" et password pour le php -->
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
               </div>
               <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter your password" required>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
               </div>
               <button type="submit" class="btn btn-primary w-100">Login</button>
             </form>
